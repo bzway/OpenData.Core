44868b9f701ad9f77c24a653c2d7d98294bdbff1
adm <adm@adm.com>
2016-11-29 11:42:59 +0800

    changes

---
+++ Modules/Bzway.Module.Wechat/Model/Request/WechatNewsMaterialCreateRequestModel.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bzway.Module.Wechat.Model
+{
+
+    public class WechatNewsMaterialCreateRequestModel
+    {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string media_id { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public int index { get; set; }
+        [JsonProperty]
+        public List<News_item> articles { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+        public class News_item
+        {
+            [JsonProperty]
+            public string title { get; set; }
+            [JsonProperty]
+            public string thumb_media_id { get; set; }
+            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+            public string thumb_url { get; set; }
+            [JsonProperty]
+            public string author { get; set; }
+            [JsonProperty]
+            public string digest { get; set; }
+            [JsonProperty]
+            public int show_cover_pic { get; set; }
+            [JsonProperty]
+            public string content { get; set; }
+            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+            public string url { get; set; }
+            [JsonProperty]
+            public string content_source_url { get; set; }
+        }
+    }
+  
+}
---
+++ Modules/Bzway.Module.Wechat/Model/Response/WeChatMaterial/WechatUploadNewsMaterialImageResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bzway.Module.Wechat.Model
+{
+    public class WechatUploadNewsMaterialImageResponseModel
+    {
+        public string  url { get; set; } 
+    }
+}
